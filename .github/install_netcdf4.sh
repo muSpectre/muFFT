46ea1dd6dae7fc4c292f82db029fa1d882226a38
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2024-09-25 21:29:46 +0200

    CI: macOS x86_64 wheels

--- .github/install_netcdf4.sh
+++ .github/install_netcdf4.sh
@@ -1,19 +1,41 @@
-if [ -z "$HOMEBREW_REPOSITORY" ]; then
+echo "uname -a is:"
+uname -a
+echo "============"
+
+echo "Environment variables are:"
+printenv
+echo "=========================="
+
+# The variable HOMEBREW_REPOSITORY appears to be only present on ARM Macs, not Intel Macs
+# HOMEBREW_NO_AUTO_UPDATE is present on both
+if [ -z "$INSTALL_PREFIX" ]; then
+if [ -z "$HOMEBREW_NO_AUTO_UPDATE" ]; then
     INSTALL_PREFIX=/usr/local
 else
     brew install pkg-config
-    INSTALL_PREFIX=$HOMEBREW_REPOSITORY
-    export PKG_CONFIG_PATH
+    INSTALL_PREFIX=$(which brew | sed 's,/bin/brew,,')
+    # libxml2 pkg-config file is not in the default path on Intel Macs
 fi
-printenv
-curl -L https://download.gnome.org/sources/libxml2/2.12/libxml2-2.12.7.tar.xz | tar -Jx
-cd libxml2-2.12.7
-./configure --without-lzma --without-python --enable-static --disable-shared --with-pic --prefix=$INSTALL_PREFIX
+fi
+
+export PATH=${INSTALL_PREFIX}/bin:${PATH}
+export PKG_CONFIG_PATH=${INSTALL_PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}
+
+echo "Installing into prefix ${INSTALL_PREFIX}..."
+echo "PATH is ${PATH}"
+echo "PKG_CONFIG_PATH is ${PKG_CONFIG_PATH}"
+
+XML2="libxml2-2.12.9"
+curl -L https://download.gnome.org/sources/libxml2/2.12/${XML2}.tar.xz | tar -Jx
+cd ${XML2}
+./configure --build=$(echo $ARCHFLAGS | sed 's/-arch //') --without-lzma --without-python --enable-static --disable-shared --with-pic --prefix=$INSTALL_PREFIX
 make
 make install
 cd ..
-curl -L https://downloads.unidata.ucar.edu/netcdf-c/4.9.2/netcdf-c-4.9.2.tar.gz | tar -zx
-cd netcdf-c-4.9.2
-./configure --disable-hdf5 --disable-byterange --enable-static --disable-shared --with-pic --prefix=$INSTALL_PREFIX
+
+NETCDF="netcdf-c-4.9.2"
+curl -L https://downloads.unidata.ucar.edu/netcdf-c/4.9.2/${NETCDF}.tar.gz | tar -zx
+cd ${NETCDF}
+./configure --build=$(echo $ARCHFLAGS | sed 's/-arch //') --disable-hdf5 --disable-byterange --enable-static --disable-shared --with-pic --prefix=$INSTALL_PREFIX
 make
 make install
