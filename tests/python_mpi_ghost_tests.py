615d4dbc334a042d7d06130b587ced95fea018a1
MartinLadecky <m.ladecky@gmail.com>
2025-11-04 13:19:24 +0100

    Adds  FAILING  unit impulse test for MPI grid decomposition
     TEST indicates the bug . THe same test passed in pure MUGRID
    This test ensures that the solver accurately computes
    the unit  impulse response and satisfies the expected zero-mean condition.

--- tests/python_mpi_ghost_tests.py
+++ tests/python_mpi_ghost_tests.py
@@ -193,3 +193,99 @@ def test_apply_stencil(engine_str):
         2 * np.pi * np.cos(2 * np.pi * (x + 0.25) / nx) / nx,
         atol=1e-5,
     )
+
+
+@pytest.mark.parametrize("engine_str", engines)
+def test_unit_impuls(engine_str):
+    # Two dimensional grid
+    nx, ny = nb_grid_pts = [4, 6]
+
+    left_ghosts = [1, 1]
+    right_ghosts = [1, 1]
+
+    try:
+        engine = muFFT.FFT(
+            nb_grid_pts,
+            engine=engine_str,
+            communicator=communicator,
+            nb_ghosts_left=left_ghosts,
+            nb_ghosts_right=right_ghosts,
+        )
+        engine.create_plan(1)
+    except muFFT.UnknownFFTEngineError:
+        # This FFT engine has not been compiled into the code. Skip
+        # test.
+        return
+
+    fc = engine.real_field_collection
+    fc.set_nb_sub_pts("quad_points", 2)
+    fc.set_nb_sub_pts("nodal_points", 1)
+
+    # Get nodal field
+    nodal_field = fc.real_field("nodal-field", (1,), "nodal_points")
+    impuls_response_field = fc.real_field("impuls_response_field", (1,), "nodal_points")
+
+    # Get quadrature field of shape (2, quad, nx, ny)
+    quad_field = fc.real_field("quad-field", (2,), "quad_points")
+
+    impuls_locations = (impuls_response_field.icoordsg[0] == 0) & (impuls_response_field.icoordsg[1] == 0)
+    left_location = (impuls_response_field.icoordsg[0] == nx - 1) & (impuls_response_field.icoordsg[1] == 0)
+    right_location = (impuls_response_field.icoordsg[0] == 1) & (impuls_response_field.icoordsg[1] == 0)
+
+    top_location = (impuls_response_field.icoordsg[0] == 0) & (impuls_response_field.icoordsg[1] == 1)
+    bottom_location = (impuls_response_field.icoordsg[0] == 0) & (impuls_response_field.icoordsg[1] == ny - 1)
+
+    nodal_field.sg[0, 0, impuls_locations] = 1
+    impuls_response_field.sg[0, 0, impuls_locations] = 4
+    impuls_response_field.sg[0, 0, left_location] = -1
+    impuls_response_field.sg[0, 0, right_location] = -1
+    impuls_response_field.sg[0, 0, top_location] = -1
+    impuls_response_field.sg[0, 0, bottom_location] = -1
+
+    print(f'unit impuls: nodal field with buffers in rank {MPI.COMM_WORLD.rank} \n ' + f'{nodal_field.s}')
+    print(
+        f'impuls_response_field: nodal field with buffers in rank {MPI.COMM_WORLD.rank} \n ' + f'{impuls_response_field.s}')
+
+    engine.communicate_ghosts(nodal_field)
+    # Derivative stencil of shape (2, quad, 2, 2)
+    gradient = np.array(
+        [
+            [  # Derivative in x-direction
+                [[[-1, 0], [1, 0]]],  # Bottom-left triangle (first quadrature point)
+                [[[0, -1], [0, 1]]],  # Top-right triangle (second quadrature point)
+            ],
+            [  # Derivative in y-direction
+                [[[-1, 1], [0, 0]]],  # Bottom-left triangle (first quadrature point)
+                [[[0, 0], [-1, 1]]],  # Top-right triangle (second quadrature point)
+            ],
+        ],
+    )
+    gradient_op = muGrid.ConvolutionOperator([0, 0], gradient)
+
+    # Apply the gradient operator to the nodal field and write result to the quad field
+    gradient_op.apply(nodal_field, quad_field)
+
+    engine.communicate_ghosts(quad_field)
+
+    # Apply the gradient transposed operator to the quad field and write result to the nodal field
+    gradient_op.transpose(quadrature_point_field=quad_field,
+                          nodal_field=nodal_field,
+                          weights=[1 / 2, 1 / 2]  # size of the element is half of the pixel. Pixel size is 1
+                          )
+
+    print(
+        f'computed unit impuls response : nodal field with buffers in rank {MPI.COMM_WORLD.rank} \n ' + f'{nodal_field.s}')
+
+    print(f'local sum on core (nodal_field.s) = {np.sum(nodal_field.s)}')  # does not have to be zero
+    local_sum = np.sum(nodal_field.s)
+    total_sum = communicator.sum(local_sum)
+    print(f'total_sum = {total_sum}')  # have to be zero
+
+    # Check that the nodal_field has zero mean
+    np.testing.assert_allclose(
+        total_sum, 0,
+        atol=1e-10, )
+    # Check that the impulse response is correct
+    np.testing.assert_allclose(
+        nodal_field.s, impuls_response_field.s,
+        atol=1e-5, )
