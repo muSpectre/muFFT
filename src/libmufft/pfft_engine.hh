770425a6f4fd51dcf2adf2fed1659c387d1f2b69
Lars Pastewka <lars.pastewka@surface.design>
2025-11-08 10:03:37 +0100

    MAINT: Cosmetics

--- src/libmufft/pfft_engine.hh
+++ src/libmufft/pfft_engine.hh
@@ -42,115 +42,120 @@
 
 namespace muFFT {
 
-  /**
-   * implements the `muFFT::FFTEngineBase` interface using the
-   * FFTW library
-   */
-  class PFFTEngine : public FFTEngineBase {
-   public:
-    using Parent = FFTEngineBase;  //!< base class
-    //! field for Fourier transform of second-order tensor
-    using FourierField_t = typename Parent::FourierField_t;
-    //! real-valued second-order tensor
-    using RealField_t = typename Parent::RealField_t;
-    //! Default constructor
-    PFFTEngine() = delete;
-
     /**
-     * Constructor with the domain's number of grid points in each direction and
-     * the communicator
-     * @param nb_grid_pts number of grid points of the global grid
-     * @param comm MPI communicator object
-     * @param plan_flags MPI planner flags
-     * @param allow_temporary_buffer allow the creation of temporary buffers
-     *        if the input buffer has the wrong memory layout
-     * @param allow_destroy_input allow that the input buffers are invalidated
-     *        during the FFT
+     * implements the `muFFT::FFTEngineBase` interface using the
+     * FFTW library
      */
-    PFFTEngine(const IntCoord_t & nb_grid_pts,
-               Communicator comm = Communicator(),
-               const FFT_PlanFlags & plan_flags = FFT_PlanFlags::estimate,
-               bool allow_temporary_buffer = true,
-               bool allow_destroy_input = false);
+    class PFFTEngine : public FFTEngineBase {
+       public:
+        using Parent = FFTEngineBase;  //!< base class
+        //! field for Fourier transform of second-order tensor
+        using FourierField_t = typename Parent::FourierField_t;
+        //! real-valued second-order tensor
+        using RealField_t = typename Parent::RealField_t;
+        //! Default constructor
+        PFFTEngine() = delete;
 
-    //! Copy constructor
-    PFFTEngine(const PFFTEngine & other) = delete;
+        /**
+         * Constructor with the domain's number of grid points in each direction
+         * and the communicator
+         * @param nb_grid_pts number of grid points of the global grid
+         * @param comm MPI communicator object
+         * @param plan_flags MPI planner flags
+         * @param allow_temporary_buffer allow the creation of temporary buffers
+         *        if the input buffer has the wrong memory layout
+         * @param allow_destroy_input allow that the input buffers are
+         * invalidated during the FFT
+         */
+        PFFTEngine(const IntCoord_t & nb_grid_pts,
+                   Communicator comm = Communicator(),
+                   const FFT_PlanFlags & plan_flags = FFT_PlanFlags::estimate,
+                   bool allow_temporary_buffer = true,
+                   bool allow_destroy_input = false);
 
-    //! Move constructor
-    PFFTEngine(PFFTEngine && other) = delete;
+        //! Copy constructor
+        PFFTEngine(const PFFTEngine & other) = delete;
 
-    //! Destructor
-    virtual ~PFFTEngine() noexcept;
+        //! Move constructor
+        PFFTEngine(PFFTEngine && other) = delete;
 
-    //! Copy assignment operator
-    PFFTEngine & operator=(const PFFTEngine & other) = delete;
+        //! Destructor
+        virtual ~PFFTEngine() noexcept;
 
-    //! Move assignment operator
-    PFFTEngine & operator=(PFFTEngine && other) = delete;
+        //! Copy assignment operator
+        PFFTEngine & operator=(const PFFTEngine & other) = delete;
 
-    // compute the plan, etc
-    void create_plan(const Index_t & nb_dof_per_pixel) override;
+        //! Move assignment operator
+        PFFTEngine & operator=(PFFTEngine && other) = delete;
 
-    //! perform a deep copy of the engine (this should never be necessary in
-    //! c++)
-    std::unique_ptr<FFTEngineBase> clone() const final;
+        // compute the plan, etc
+        void create_plan(const Index_t & nb_dof_per_pixel) override;
 
-    /**
-     * need to override this method here, since FFTWMPI requires field padding
-     */
-    RealField_t &
-    register_real_space_field(const std::string & unique_name,
-                              const Index_t & nb_dof_per_pixel) final;
+        //! perform a deep copy of the engine (this should never be necessary in
+        //! c++)
+        std::unique_ptr<FFTEngineBase> clone() const final;
 
-    /**
-     * need to override this method here, since FFTWMPI requires field padding
-     */
-    RealField_t &
-    register_real_space_field(const std::string & unique_name,
-                              const Shape_t & shape) final;
+        /**
+         * need to override this method here, since FFTWMPI requires field
+         * padding
+         */
+        RealField_t &
+        register_real_space_field(const std::string & unique_name,
+                                  const Index_t & nb_dof_per_pixel) final;
 
-    /**
-     * need to override this method here, since FFTWMPI requires field padding
-     */
-    muGrid::ComplexField &
-    register_fourier_space_field(const std::string & unique_name,
-                                 const Index_t & nb_dof_per_pixel) final;
+        /**
+         * need to override this method here, since FFTWMPI requires field
+         * padding
+         */
+        RealField_t &
+        register_real_space_field(const std::string & unique_name,
+                                  const Shape_t & shape) final;
 
-    /**
-     * need to override this method here, since FFTWMPI requires field padding
-     */
-    muGrid::ComplexField &
-    register_fourier_space_field(const std::string & unique_name,
-                                 const Shape_t & shape) final;
+        /**
+         * need to override this method here, since FFTWMPI requires field
+         * padding
+         */
+        muGrid::ComplexField &
+        register_fourier_space_field(const std::string & unique_name,
+                                     const Index_t & nb_dof_per_pixel) final;
 
-   protected:
-    //! forward transform
-    void compute_fft(const RealField_t & field,
-                     FourierField_t & output_field) override;
+        /**
+         * need to override this method here, since FFTWMPI requires field
+         * padding
+         */
+        muGrid::ComplexField &
+        register_fourier_space_field(const std::string & unique_name,
+                                     const Shape_t & shape) final;
 
-    //! inverse transform
-    void compute_ifft(const FourierField_t & input_field,
-                      RealField_t & output_field) override;
+       protected:
+        //! forward transform
+        void compute_fft(const RealField_t & field,
+                         FourierField_t & output_field) override;
 
-    //! check whether real-space buffer has the correct memory layout
-    bool check_real_space_field(const RealField_t & field,
-                                FFTDirection direction) const final;
+        //! inverse transform
+        void compute_ifft(const FourierField_t & input_field,
+                          RealField_t & output_field) override;
 
-    //! check whether Fourier-space buffer has the correct memory layout
-    bool check_fourier_space_field(const FourierField_t & field,
-                                   FFTDirection direction) const final;
+        //! check whether real-space buffer has the correct memory layout
+        bool check_real_space_field(const RealField_t & field,
+                                    FFTDirection direction) const final;
 
-    static int nb_engines;  //!< number of times this engine has
-                            //!< been instantiated
-    MPI_Comm mpi_comm;  //!< MPI communicator
-    //! holds the plans for forward fourier transforms
-    std::map<Index_t, pfft_plan> fft_plans{};
-    //! holds the plans for inversefourier transforms
-    std::map<Index_t, pfft_plan> ifft_plans{};
-    //! holds the fourier field sizes including padding for different transforms
-    std::map<Index_t, Index_t> required_workspace_sizes{};
-    bool active{true};  //!< PFFT sometimes assigns zero grid points
-  };
+        //! check whether Fourier-space buffer has the correct memory layout
+        bool check_fourier_space_field(const FourierField_t & field,
+                                       FFTDirection direction) const final;
+
+        static int nb_engines;  //!< number of times this engine has
+                                //!< been instantiated
+        MPI_Comm mpi_comm;      //!< MPI communicator
+        //! holds the plans for forward fourier transforms
+        std::map<Index_t, pfft_plan> fft_plans{};
+        //! holds the plans for inversefourier transforms
+        std::map<Index_t, pfft_plan> ifft_plans{};
+        //! holds the fourier field sizes including padding for different
+        //! transforms
+        std::map<Index_t, Index_t> required_workspace_sizes{};
+        bool active{true};  //!< PFFT sometimes assigns zero grid points
+    };
 
 }  // namespace muFFT
 
